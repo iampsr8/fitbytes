ff5c5f73b41a3079b5c667f05f3c68fe3b31049f
Prateek Rath <prateekrath28@gmail.com>
2022-09-06 01:38:23 +0530

    create loader footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from '../assets/images/fitbytes2.png'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgcolor="#FFF3F4">
+      <Stack gap="20px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px"> 
+        <img src={Logo} alt="logo" style={{ width: '200px', height: '35px' }} />
+        <Typography variant="h5" fontWeight="bold" sx={{ fontSize: { lg: '25px', xs: '18px' } }} mt="10px" textAlign="center" pb="40px">
+          Made with 💪 by Prateek Rath
+        </Typography>
+      </Stack>
+      
+    </Box>
   )
 }
 
